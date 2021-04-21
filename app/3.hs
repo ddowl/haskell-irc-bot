13f86401eb7fa3abb002491a252c18662f9f5eb0
Drew Dowling <drewdowling37@gmail.com>
2021-04-20 23:04:36 -0700

    Bot3

---
+++ app/3.hs
@@ -0,0 +1,60 @@
+import Data.List
+import qualified Network.Socket as N
+import System.Exit
+import System.IO
+
+-- Configuration options
+myServer = "irc.freenode.org" :: String
+
+myPort = 6667 :: N.PortNumber
+
+myChan = "#tutbot-testing" :: String
+
+myNick = "tutbot" :: String
+
+-- Toplevel program
+main :: IO ()
+main = do
+  h <- connectTo myServer myPort
+  write h "NICK" myNick
+  write h "USER" (myNick ++ " 0 * :tutorial bot")
+  write h "JOIN" myChan
+  listen h
+
+-- Connect to a server given its name and port
+connectTo :: N.HostName -> N.PortNumber -> IO Handle
+connectTo host port = do
+  addr : _ <- N.getAddrInfo Nothing (Just host) (Just (show port))
+  sock <- N.socket (N.addrFamily addr) (N.addrSocketType addr) (N.addrProtocol addr)
+  N.connect sock (N.addrAddress addr)
+  N.socketToHandle sock ReadWriteMode
+
+-- Send a message to a handle
+write :: Handle -> String -> String -> IO ()
+write h cmd args = do
+  let msg = cmd ++ " " ++ args ++ "\r\n"
+  hPutStr h msg -- Send message on the wire
+  putStr ("> " ++ msg) -- Show sent message on the command line
+
+-- Process each line from the server
+listen :: Handle -> IO ()
+listen h = forever $ do
+  line <- hGetLine h
+  putStrLn line
+  let s = init line
+  if isPing s then pong s else eval h (clean s)
+  where
+    forever a = do a; forever a
+    clean = drop 1 . dropWhile (/= ':') . drop 1
+    isPing x = "PING :" `isPrefixOf` x
+    pong x = write h "PONG" (':' : drop 6 x)
+
+-- Dispatch a command
+eval :: Handle -> String -> IO ()
+eval h "!quit" = write h "QUIT" ":Exiting" >> exitSuccess
+eval h x | "!id " `isPrefixOf` x = privmsg h (drop 4 x)
+eval _ _ = return () -- ignore everything else
+
+-- Send a privmsg to the channel
+privmsg :: Handle -> String -> IO ()
+privmsg h s = write h "PRIVMSG" (myChan ++ " :" ++ s)
